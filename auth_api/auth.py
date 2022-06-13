80da79a515b2f63c9299caad8a1c8ddb6519d1e8
Hugo Moinheiro <hugo.moinheiro@ua.pt>
2022-06-13 23:05:33 +0100

    redirecting working

--- auth_api/auth.py
+++ auth_api/auth.py
@@ -83,6 +83,8 @@ dbcon.commit()
 
 dbcon.execute('''INSERT OR IGNORE INTO USERS(username, password, website)
                  VALUES ('admin',  'admin', '')''')
+dbcon.execute('''INSERT OR IGNORE INTO USERS(username, password, website)
+                 VALUES ('admin',  'admin', 'test.url')''')
 
 allData = cursor.execute('''SELECT * FROM USERS''')
 allAccounts = cursor.fetchall()
@@ -105,12 +107,9 @@ def runApi():
                         }
                        })
     api = Api(app)
-    global redirectUrl
-    redirectUrl = ''
 
     @app.get('/signup')
     def signupPage():
-        global redirectUrl
         redirectUrl = ''
         d = str(request.data.decode('utf-8'))
         try:
@@ -122,26 +121,25 @@ def runApi():
             pass
         if redirectUrl == None or redirectUrl == '':
             redirectUrl = str(request.environ['REMOTE_ADDR'])
-        return render_template('signup.html')
+        if TEST: redirectUrl = 'test.url'
+        return render_template('signup.html', result = redirectUrl)
 
     @app.post('/signup')
     def signup():
-        global redirectUrl
         try:
             token = generateRandom()
             username = ''
             password = ''
             timestamp = ''
-            if (TEST): redirectUrl = ''
             try:
                 for key, value in request.form.items():
                     print("key: {0}, value: {1}".format(key, value))
                     if key == 'username': username = value
                     if key == 'password': password = value
+                    if key == 'redirectUrl': redirectUrl = value
             except:
                 response = redirect('/signup', 400)
                 return response
-                # return Response(status=400)
             token = generateRandom()
             timestamp = str(time.asctime(time.localtime(time.time())))
             accData = cursor.execute('''SELECT * FROM USERS WHERE username = "%s" AND website = "%s"'''%(username, redirectUrl))
@@ -154,6 +152,7 @@ def runApi():
                                     VALUES ("%s", "%s", "%s", "%s", "%s")'''%(username, password, redirectUrl, token, timestamp))
             print('Username: ' + str(username))
             print('Pass: ' + str(password))
+            print('redirectUrl: ' + str(redirectUrl))
             # print('Token: ' + str(token))
             allData = cursor.execute('''SELECT * FROM USERS''')
             allAccounts = cursor.fetchall()
@@ -162,6 +161,7 @@ def runApi():
             # response = {'token' : token }
             # r = Response(json.dumps(response), status=201, mimetype='application/json')
             # return redirect(redirectUrl, 201, None)
+            redirectUrl = 'https://'+redirectUrl+'/'
             response = redirect(redirectUrl, 201, None)
             response.headers = {'token' : token}
             return response
@@ -172,7 +172,6 @@ def runApi():
 
     @app.get('/login')
     def loginPage():
-        global redirectUrl
         redirectUrl = ''
         #print(request.headers)
         redirectUrl=request.headers.get('Referer')
@@ -187,24 +186,24 @@ def runApi():
             pass
         if redirectUrl == None or redirectUrl == '':
             redirectUrl = str(request.environ['REMOTE_ADDR'])
-        redir.append(redirectUrl)
-        print("Redirect to:"+redir[0])
-        return render_template('login.html')
+        if TEST: redirectUrl = 'test.url'
+        # redir.append(redirectUrl)
+        # print("Redirect to:"+redir[0])
+        return render_template('login.html', result = redirectUrl)
 
     @app.post('/login')
     def login():
-        global redirectUrl
         try:
             token = generateRandom()
             username = ''
             password = ''
             timestamp = ''
-            if (TEST): redirectUrl = ''
             try:
                 for key, value in request.form.items():
-                    # print("key: {0}, value: {1}".format(key, value))
+                    print("key: {0}, value: {1}".format(key, value))
                     if key == 'username': username = value
                     if key == 'password': password = value
+                    if key == 'redirectUrl': redirectUrl = value
             except:
                 response = redirect('/login', 400)
                 return response
@@ -229,17 +228,19 @@ def runApi():
             # response = {'token' : token }
             # r = Response(json.dumps(response), status=201, mimetype='application/json')
             # return redirect(redirectUrl, 201, None)
-            redirectUrl=redir.pop(0)
-            print("Redirect to:"+redirectUrl)
+            # redirectUrl=redir.pop(0)
+            # print("Redirect to:"+redirectUrl)
             #response = redirect(redirectUrl, code=302)
             #headers = {'token' : token}
             headers = 'token_' + token + '_user_'+username
             #print(response.headers)
+            redirectUrl = 'https://'+redirectUrl+'/'
             return test(redirectUrl,headers)
         except:
             response = redirect('/login', 500)
             return response
             # return Response(status=500)
+
     @app.route('/test')
     def test(redirectUrl,headers):
         print(redirectUrl+'params/'+headers)
@@ -262,7 +263,7 @@ def runApi():
                 username = data['username']
                 password = data['password']
                 redirectUrl = data['redirectUrl']
-                if (TEST): redirectUrl = ''
+                if TEST: redirectUrl = 'test.url'
             except:
                 return Response(status=400)
             timestamp = str(time.asctime(time.localtime(time.time())))
@@ -364,7 +365,7 @@ def runApi():
                 username = data['username']
                 password = data['password']
                 redirectUrl = data['redirectUrl']
-                if (TEST): redirectUrl = ''
+                if TEST: redirectUrl = 'test.url'
             except:
                 return Response(status=400)
             accData = cursor.execute('''SELECT * FROM USERS WHERE token = "%s"'''%(token))
@@ -400,7 +401,7 @@ def runApi():
                 username = data['username']
                 password = data['password']
                 redirectUrl = data['redirectUrl']
-                if (TEST): redirectUrl = ''
+                if TEST: redirectUrl = 'test.url'
             except:
                 return Response(status=400)
             timestamp = str(time.asctime(time.localtime(time.time())))
