2c4d60f3c9112a6cdecf80d512a62aa8cc63928a
CPinhoK <pauloricardopinho@ua.pt>
2022-06-22 15:32:11 +0100

    remove copies

--- payment_service/auth_copy/app-deploy.sh
+++
@@ -1,3 +0,0 @@
-sudo bash docker.sh
-
-bash sapp.sh
--- payment_service/auth_copy/docker.sh
+++
@@ -1,3 +0,0 @@
-docker build -t registry.deti:5000/hugom/auth-api:latest -f Dockerfile.app --no-cache .
-
-docker push registry.deti:5000/hugom/auth-api:latest
